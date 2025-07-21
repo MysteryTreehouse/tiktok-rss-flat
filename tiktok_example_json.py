7b773b65069d08c0f1b1b777981c7d9c88ff2f60
MysteryTreehouse <deadwillieprice@gmail.com>
2025-07-21 14:22:36 -0700

    Update tiktok_example_json.py

--- tiktok_example_json.py
+++ tiktok_example_json.py
@@ -1,18 +1,19 @@
 from TikTokApi import TikTokApi
 import json
 
-api = TikTokApi.get_instance()
+# ← pass your proxy + browser settings here
+api = TikTokApi.get_instance(
+    browser="webkit",
+    headless=False,
+    proxy="http://user:pass@your.proxy.host:PORT"
+)
 
 count = 1
-
 tiktoks = api.by_username("iamtabithabrown", count=count)
 
 jsonString = json.dumps(tiktoks)
-jsonFile = open("tiktok_example_data.json", "w")
-jsonFile.write(jsonString)
-jsonFile.close()
+with open("tiktok_example_data.json", "w") as jsonFile:
+    jsonFile.write(jsonString)
 
 for tiktok in tiktoks:
-   # print(tiktok)
-    print(tiktok['video']['cover'])
-    
+    print(tiktok["video"]["cover"])
